ce8f021b827d8fdb9c44c5c001727871b5aed2e9
Jan Biedermann <jan@kursator.de>
2019-10-07 12:51:57 +0200

    add isomorfeus-react benchmarks

---
+++ frameworks/keyed/isomorfeus-react/src/isomorfeus/components/row.rb
@@ -0,0 +1,19 @@
+class Row < React::PureComponent::Base
+  event_handler :on_select do
+    props.select.call(props.item)
+  end
+
+  event_handler :on_remove do
+    props.remove.call(props.item)
+  end
+
+  render do
+    item = props.item
+    TR(class_name: (props.selected ? "danger" : "")) do
+      TD(class_name: "col-md-1") { item[:id] }
+      TD(class_name: "col-md-4") { A(on_click: :on_select) { item[:label] }}
+      TD(class_name: "col-md-1") { A(on_click: :on_remove) { SPAN(class_name: "glyphicon glyphicon-remove", aria_hidden: "true") }}
+      TD(class_name: "col-md-6")
+    end
+  end
+end
