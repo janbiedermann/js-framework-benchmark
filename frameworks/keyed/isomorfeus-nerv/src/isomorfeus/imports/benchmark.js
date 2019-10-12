10970e1c3e38329f9948cc372a70c95be091ada4
Jan Biedermann <jan@kursator.de>
2019-10-12 14:34:39 +0200

    add i-nerv, i-preact

---
+++ frameworks/keyed/isomorfeus-nerv/src/isomorfeus/imports/benchmark.js
@@ -0,0 +1,15 @@
+// entry file for the browser environment
+
+// import npm modules that are valid to use only in the browser
+import * as Redux from 'redux';
+global.Redux = Redux;
+import React from 'react';
+global.React = React;
+import ReactDOM from 'react-dom';
+global.ReactDOM = ReactDOM;
+
+import init_app from 'benchmark_loader.rb';
+init_app();
+Opal.load('benchmark_loader');
+
+
---
+++ frameworks/keyed/isomorfeus-preact/src/isomorfeus/benchmark_loader.rb
@@ -0,0 +1,9 @@
+require 'opal'
+require 'opal-activesupport'
+require 'opal-autoloader'
+require 'isomorfeus-redux'
+require 'isomorfeus-react'
+
+require_tree 'components'
+
+Isomorfeus.start_app!
