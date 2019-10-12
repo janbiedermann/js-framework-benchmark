c0f282363c590c9f990efe7a450a871d45b3a307
Jan Biedermann <jan@kursator.de>
2019-10-12 19:17:14 +0200

    update

---
+++ frameworks/keyed/isomorfeus-lucid-nerv/src/webpack/production.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const OwlResolver = require('opal-webpack-loader/resolver');
+
+const common_config = {
+    target: 'web',
+    context: path.resolve(__dirname, '../isomorfeus'),
+    mode: "production",
+    optimization: {
+        minimize: true
+    },
+    performance: {
+        maxAssetSize: 20000000,
+        maxEntrypointSize: 20000000
+    },
+    resolve: {
+        plugins: [
+            new OwlResolver('resolve', 'resolved') // resolve ruby files
+        ],
+        alias: {
+            'react': 'nervjs',
+            'react-dom': 'nervjs',
+        }
+    },
+    module: {
+        rules: [
+            {
+                // opal-webpack-loader will compile and include ruby files in the pack
+                test: /(\.js)?\.rb$/,
+                use: [
+                    {
+                        loader: 'opal-webpack-loader',
+                        options: {
+                            sourceMap: false,
+                            hmr: false
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
+
+const bench_config = {
+    entry: {
+        "benchmark": [path.resolve(__dirname, '../isomorfeus/imports/benchmark.js')],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '../../dist'),
+        publicPath: '/'
+    }
+};
+
+const benchmark = Object.assign({}, common_config, bench_config);
+
+module.exports = [ benchmark ];
